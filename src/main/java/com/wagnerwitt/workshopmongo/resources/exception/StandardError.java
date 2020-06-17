d570536a45f7a92cc6170f5821706966ce96e895
Wagner Witt <wittwizard@gmail.com>
2020-06-17 15:28:04 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/wagnerwitt/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,65 @@
+package com.wagnerwitt.workshopmongo.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String messagem;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public StandardError(Long timestamp, Integer status, String error, String messagem, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.messagem = messagem;
+		this.path = path;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessagem() {
+		return messagem;
+	}
+
+	public void setMessagem(String messagem) {
+		this.messagem = messagem;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
--- src/main/java/com/wagnerwitt/workshopmongo/services/UserService.java
+++ src/main/java/com/wagnerwitt/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wagnerwitt.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wagnerwitt.workshopmongo.domain.User;
 import com.wagnerwitt.workshopmongo.repository.UserRepository;
+import com.wagnerwitt.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }

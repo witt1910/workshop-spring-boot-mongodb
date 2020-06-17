26155cf5c93e26a11b3c8439b0a76e5b0d43106c
Wagner Witt <wittwizard@gmail.com>
2020-06-17 19:05:46 -0300

    Insercao de usuario com POST

--- src/main/java/com/wagnerwitt/workshopmongo/services/UserService.java
+++ src/main/java/com/wagnerwitt/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wagnerwitt.workshopmongo.domain.User;
+import com.wagnerwitt.workshopmongo.dto.UserDTO;
 import com.wagnerwitt.workshopmongo.repository.UserRepository;
 import com.wagnerwitt.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(),objDTO.getEmail());
+	}
 }

4b5ac1864fec2fffd842de656d70f144fffdc0d6
Wagner Witt <wittwizard@gmail.com>
2020-06-17 19:18:58 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/wagnerwitt/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wagnerwitt/workshopmongo/resources/UserResource.java
@@ -38,8 +38,8 @@ public class UserResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
-		User obj = service.fromDTO(objDTO);
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
@@ -48,6 +48,14 @@ public class UserResource {
 	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }

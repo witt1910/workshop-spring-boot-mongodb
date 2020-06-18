0d0369792377eca5275aea046842834d8474623a
Wagner Witt <wittwizard@gmail.com>
2020-06-17 23:47:32 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/wagnerwitt/workshopmongo/domain/Post.java
+++ src/main/java/com/wagnerwitt/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.wagnerwitt.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.wagnerwitt.workshopmongo.dto.AuthorDTO;
+import com.wagnerwitt.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -94,4 +107,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 }
